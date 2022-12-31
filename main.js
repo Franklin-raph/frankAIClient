77dcf95436f63749906a6065b93b7e1503414d3e
Franklin-raph <igboekwulusifranklin@gmail.com>
2022-12-31 15:54:23 +0100

    updated the UI

--- main.js
+++ main.js
@@ -4,6 +4,23 @@ const form = document.querySelector("form");
 const humanRequest = document.querySelector('.humanRequest')
 const botAnswer = document.querySelector('.botAnswer')
 
+const botImageLoader = document.querySelector('.botImageLoader')
+
+const humanImageRequest = document.querySelector('.humanImageRequest')
+const botImageAnswer = document.querySelector('.botImageAnswer')
+const selectedItems = document.querySelectorAll('.item')
+
+const createImagePage = document.getElementById("createImage")
+const createCompletionPage = document.getElementById("createCompletion")
+
+const createImageText = document.querySelector("#createImage #textArea")
+
+const modal = document.querySelector(".modal")
+
+const modalClose = document.querySelector('.modalClose').addEventListener('click', ()=> {
+  modal.classList.remove('open')
+})
+
 let loadInterval;
 
 function botLoader(element) {
@@ -31,13 +48,12 @@ function botTyping(element, text){
   }, 20)
 }
 
-async function handleSubmit (e){
+async function handleCreateCompletion (e){
   e.preventDefault()
-  
   const formData = new FormData(form)
   form.reset()
-  
   if (!formData.get("prompt")) {
+    modal.classList.add('open')
     return;
   }
 
@@ -61,10 +77,78 @@ async function handleSubmit (e){
   if (response.ok) {
     clearInterval(loadInterval);
     botTyping(botAnswer, data.trim())
+    console.log(data.trim())
 
   } else {
     clearInterval(loadInterval);
     botTyping(botAnswer, "Oops!! something went wrong. Please try again later")
   }
 }
-form.addEventListener('submit', handleSubmit)
+
+async function handleImageGeneration(){
+  if (!createImageText.value) {
+    modal.classList.add('open')
+    return;
+  }
+
+  botImageAnswer.src = ""
+  botLoader(botImageLoader)
+  
+  humanImageRequest.textContent = ""
+  humanImageRequest.textContent = createImageText.value
+
+  console.log(createImageText.value)
+  chatArea.scrollTop = chatArea.scrollHeight
+  const response = await fetch('https://frankai.onrender.com/openai/imageGeneration', {
+      method: "POST",
+      body: JSON.stringify({
+        prompt: createImageText.value
+      }),
+      headers: {
+          'Content-Type':'application/json',
+      }
+  })
+
+  createImageText.value = ""
+  
+  const { data } = await response.json();
+  console.log(response)
+  if (response.ok) {
+    clearInterval(loadInterval);
+    botImageAnswer.src = data
+    console.log(data)
+
+  } else {
+    clearInterval(loadInterval);
+    botTyping(botImageLoader, "Oops!! something went wrong. Please try again later")
+  }
+}
+
+document.querySelector('.createCompletionBtn').addEventListener('click', handleCreateCompletion)
+
+document.querySelector('.handleImageGenerationBtn').addEventListener('click', (e)=>{
+  e.preventDefault()
+  handleImageGeneration()
+})
+
+
+
+
+selectedItems.forEach((selectedItem, index) => {
+  selectedItem.addEventListener('click', ()=>{
+    selectedItems.forEach(unselectedBtn =>{
+      unselectedBtn.classList.remove('activeItem')
+    });
+    selectedItems[index].classList.add('activeItem')
+
+    if(selectedItem.innerText === "Generate Images"){
+      createImagePage.style.display = "block"
+      createCompletionPage.style.display = "none"
+    }
+
+    if(selectedItem.innerText === "Ask Me"){
+      createImagePage.style.display = "none"
+      createCompletionPage.style.display = "block"
+    }
+  })
+})
